a01d78eb0197bb776a0cbea56a014ee35ddd95a2
Klim Nikishin <klimnikishinofficial@gmail.com>
2020-12-06 16:19:23 +0300

    commit com

---
+++ NetworkCodable/Model/NetworkInfo.swift
@@ -0,0 +1,24 @@
+//
+//  MetroStation.swift
+//  NetworkCodable
+//
+//  Created by Клим on 30.11.2020.
+//
+
+import Foundation
+
+struct NetworkInfo: Codable{
+    var lines: [Line]
+}
+
+struct Line: Codable{
+    var name: String
+    var stations: [Station]
+}
+
+struct Station: Codable{
+    var id: String
+    var name: String
+    var lat: Float
+    var lng: Float
+}
